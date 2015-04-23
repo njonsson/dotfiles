b0206ce2316fc05be5399b3d9d452cb893a738ac
Nils Jonsson <nils@alumni.rice.edu>
2015-04-23 16:03:30 -0600

    Correctly pass arguments from tmux IDE script to subscripts

--- resources/tmux-ide.sh
+++ resources/tmux-ide.sh
@@ -5,13 +5,13 @@ javascript_files_count=`find . -name '*.js'  | wc -l`
 ruby_files_count=`      find . -name '*.rb'  | wc -l`
 if [ $javascript_files_count -lt $clojure_files_count ] &&
    [ $ruby_files_count       -lt $clojure_files_count ]; then
-  tmux-ide-clojure "$*"
+  tmux-ide-clojure "$@"
 elif [ $clojure_files_count -lt $javascript_files_count ] &&
      [ $ruby_files_count    -lt $javascript_files_count ]; then
-  tmux-ide-javascript "$*"
+  tmux-ide-javascript "$@"
 elif [ $clojure_files_count    -lt $ruby_files_count ] &&
      [ $javascript_files_count -lt $ruby_files_count ]; then
-  tmux-ide-ruby "$*"
+  tmux-ide-ruby "$@"
 else
   echo 'Could not automatically detect a Clojure, JavaScript, or Ruby project' 1>&2
   exit 1
