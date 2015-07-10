1766ed41196d77161716b2cb5c5e6b6939e42bc3
Nils Jonsson <nils@alumni.rice.edu>
2015-07-10 11:25:19 -0500

    Add Git subcommand `ok-to-force-push` for comparing a branch to a remote

---
+++ resources/git-ok-to-force-push.sh
@@ -0,0 +1,99 @@
+#! /usr/bin/env sh
+
+set -o pipefail
+
+display_usage() {
+  printf "Usage: $(basename $0) [REMOTE] [BRANCH]\n"
+}
+
+fetch_local_commit_hash() {
+  local result=$(git show --format=%H --no-patch "$remote/$branch" 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    printf "$result"
+    return
+  fi
+
+  printf "\033[31mError!\033[0m " >&2
+  git show --format=%H --no-patch "$remote/$branch" 2>&1 | head -1
+  return 3
+}
+
+fetch_remote_commit_hash() {
+  git ls-remote --exit-code "$remote" "$branch" &>/dev/null
+  if [ $? -eq 0 ]; then
+    git ls-remote "$remote" "$branch" | cut -f1
+    return
+  fi
+
+  printf "\033[31mError!\033[0m " >&2
+  local error=$(git ls-remote "$remote" "$branch" 2>&1)
+  if [ "$error" == '' ]; then
+    local error="no branch \033[4m$branch\033[0m on remote \033[4m$remote\033[0m"
+  fi
+  printf "$error\n" >&2
+  return 2
+}
+
+main() {
+  parse_arguments "$@"
+
+  # Using `local` always sets `$?` to 0, so work around that.
+  _result=$(fetch_remote_commit_hash)
+  local status=$?
+  local remote_commit_hash="$_result"
+  unset _result
+
+  if [ $status -ne 0 ]; then
+    exit $status
+  fi
+
+  # Using `local` always sets `$?` to 0, so work around that.
+  _result=$(fetch_local_commit_hash)
+  local status=$?
+  local local_commit_hash="$_result"
+  unset _result
+
+  if [ $status -ne 0 ]; then
+    exit $status
+  fi
+
+  if [ "$remote_commit_hash" == "$local_commit_hash" ]; then
+    printf "\033[32mOK\033[0m because \033[4m$remote\033[0m’s $remote_commit_hash == \033[4m$branch\033[0m’s\n"
+    exit 0
+  else
+    printf "\033[31mFetch required\033[0m because \033[4m$remote\033[0m’s $remote_commit_hash != local $local_commit_hash\n"
+    exit 1
+  fi
+}
+
+parse_arguments() {
+  case $# in
+    0)
+      remote=origin
+      branch=$(git rev-parse --abbrev-ref HEAD)
+      ;;
+    1)
+      remote=origin
+      branch="$1"
+      ;;
+    2)
+      remote="$1"
+      branch="$2"
+      ;;
+    *)
+      display_usage
+      exit -1
+      ;;
+  esac
+
+  for argument in $@; do
+    case "$argument" in
+      --help | -h)
+        display_usage
+        exit 0
+        ;;
+    esac
+  done
+}
+
+main "$@"
