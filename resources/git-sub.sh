3ab39a7d8f84e247ca7c3d768b605f310ce9823c
Nils Jonsson <nils@alumni.rice.edu>
2015-07-10 08:26:56 -0500

    Improve Git subcommand `sub`, making it safer and easier to use
    * Deal with spaces in filenames
    * Echo back what we're doing

--- resources/git-sub.sh
+++ resources/git-sub.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env sh
 
-find=$1
-replace=$2
+find="$1"
+replace="$2"
+echo "Replacing all occurrences of '$find' with '$replace'"
 for file in $(git grep --name-only "$find")
 do
-  sed -i '' "s/$find/$replace/g" $file
+  sed -i '' "s/$find/$replace/g" "$file"
 done
