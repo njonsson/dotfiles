18ea8dbc85c8b23b44abdaa4420eef1b47ee62fe
Nils Jonsson <nils@alumni.rice.edu>
2015-07-11 20:13:18 -0500

    Enhance Git subcommand `ok-to-force-push` to handle environment variables

--- resources/git-ok-to-force-push.sh
+++ resources/git-ok-to-force-push.sh
@@ -7,28 +7,28 @@ display_usage() {
 }
 
 fetch_local_commit_hash() {
-  local result=$(git show --format=%H --no-patch "$remote/$branch" 2>/dev/null)
+  local result=$(git show --format=%H --no-patch "$REMOTE/$BRANCH" 2>/dev/null)
   if [ $? -eq 0 ]; then
     printf "$result"
     return
   fi
 
   printf "\033[31mError!\033[0m " >&2
-  git show --format=%H --no-patch "$remote/$branch" 2>&1 | head -1
+  git show --format=%H --no-patch "$REMOTE/$BRANCH" 2>&1 | head -1
   return 3
 }
 
 fetch_remote_commit_hash() {
-  git ls-remote --exit-code "$remote" "$branch" &>/dev/null
+  git ls-remote --exit-code "$REMOTE" "$BRANCH" &>/dev/null
   if [ $? -eq 0 ]; then
-    git ls-remote "$remote" "$branch" | cut -f1
+    git ls-remote "$REMOTE" "$BRANCH" | cut -f1
     return
   fi
 
   printf "\033[31mError!\033[0m " >&2
-  local error=$(git ls-remote "$remote" "$branch" 2>&1)
+  local error=$(git ls-remote "$REMOTE" "$BRANCH" 2>&1)
   if [ "$error" == '' ]; then
-    local error="no branch \033[4m$branch\033[0m on remote \033[4m$remote\033[0m"
+    local error="no branch \033[4m$BRANCH\033[0m on remote \033[4m$REMOTE\033[0m"
   fi
   printf "$error\n" >&2
   return 2
@@ -60,14 +60,14 @@ main() {
   if [ "$remote_commit_hash" == "$local_commit_hash" ]; then
     printf "\033[32mOK\033[0m"
     if [ "$VERBOSE" == 'true' ]; then
-      printf " because \033[4m$remote\033[0m’s $remote_commit_hash == \033[4m$branch\033[0m’s"
+      printf " because \033[4m$REMOTE\033[0m’s $remote_commit_hash == \033[4m$BRANCH\033[0m’s"
     fi
     printf "\n"
     exit 0
   else
     printf "\033[31mFetch required\033[0m"
     if [ "$VERBOSE" == 'true' ]; then
-      printf " because \033[4m$remote\033[0m’s $remote_commit_hash != local $local_commit_hash"
+      printf " because \033[4m$REMOTE\033[0m’s $remote_commit_hash != local $local_commit_hash"
     fi
     printf "\n"
     exit 1
@@ -75,6 +75,13 @@ main() {
 }
 
 parse_arguments() {
+  if [ "$REMOTE" != '' ]; then
+    printf "Using \$REMOTE of \033[4m$REMOTE\033[0m from environment\n"
+  fi
+  if [ "$BRANCH" != '' ]; then
+    printf "Using \$BRANCH of \033[4m$BRANCH\033[0m from environment\n"
+  fi
+
   for argument in $@; do
     case "$argument" in
       --help | -h)
@@ -85,12 +92,13 @@ parse_arguments() {
         VERBOSE=true
         ;;
       *)
-        if [ "$branch" == '' ]; then
-          branch="$argument"
-        elif [ "$remote" == '' ]; then
-          remote="$branch"
-          branch="$argument"
+        if [ "$BRANCH" == '' ]; then
+          BRANCH="$argument"
+        elif [ "$REMOTE" == '' ]; then
+          REMOTE="$BRANCH"
+          BRANCH="$argument"
         else
+          printf "\033[31mExtra argument \033[4m$argument\033[0;31m was supplied\033[0m\n" >&2
           display_usage
           exit -1
         fi
@@ -98,13 +106,13 @@ parse_arguments() {
     esac
   done
 
-  if [ "$remote" == '' ]; then
-    remote=origin
+  if [ "$REMOTE" == '' ]; then
+    REMOTE=origin
   fi
 
-  if [ "$branch" == '' ]; then
-    branch="$(git rev-parse --abbrev-ref HEAD)"
-    if [ "$branch" == 'HEAD' ]; then
+  if [ "$BRANCH" == '' ]; then
+    BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+    if [ "$BRANCH" == 'HEAD' ]; then
       printf "\033[31mNo branch appears to be checked out\033[0m\n" >&2
       exit 4
     fi
