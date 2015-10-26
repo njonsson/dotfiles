af373cce2eec29e24f9dddde2dbf2a8e13f61da2
Nils Jonsson <nils@alumni.rice.edu>
2015-10-26 11:51:29 -0500

    Enhance the tmux IDE script for Elixir programming
    * Detect ESpec examples or ExUnit tests
    * Use fswatch to run examples or tests when files change

--- resources/tmux-ide-elixir.sh
+++ resources/tmux-ide-elixir.sh
@@ -19,22 +19,6 @@ esac
 
 echo Setting up tmux IDE for Elixir
 
-bundler=''
-bundle check >/dev/null 2>&1
-case $? in
-  # Exit code 0: bundle is defined and installed
-  # Exit code 1: bundle is defined but not installed
-  0|1 )
-    echo "* Detected Bundler configuration"
-    bundler=true
-    ;;
-esac
-
-bundle_exec=""
-if [ $bundler ]; then
-  bundle_exec="bundle exec"
-fi
-
 session_name=`basename $(pwd)`
 tmux_cmd="tmux -S /var/tmux/$session_name"
 $tmux_cmd new-session -s $session_name -d
@@ -44,18 +28,40 @@ $tmux_cmd send-keys -t $session_name:1.1 C-m 'vim .' C-m
 
 $tmux_cmd split-window -h -p 40 -t $session_name:1.1
 
-guard=''
-if [ -e Guardfile ]; then
-  which guard >/dev/null
-  if [ $? = 0 ]; then
-    guard=true
-  fi
+if [ -d spec ]; then
+  espec_files_count="`find spec -name '*.ex' -o -name '*.exs' | wc -l`"
+else
+  espec_files_count=0
 fi
-if [ $guard ]; then
-  echo "* Detected Guard configuration"
-  $tmux_cmd send-keys -t $session_name:1.2 "$bundle_exec guard" C-m
-  $tmux_cmd split-window -v -p 40 -t $session_name:1.2
+if [ -d test ]; then
+  exunit_files_count="`find test -name '*.ex' -o -name '*.exs' | wc -l`"
+else
+  exunit_files_count=0
 fi
+if [ "$espec_files_count" -lt "$exunit_files_count" ]; then
+  echo "* Detected no ESpec examples so running ExUnit tests instead"
+  mix_test_cmd="mix test"
+else
+  echo "* Detected ESpec examples"
+  mix_test_cmd="mix espec"
+fi
+
+fswatch=''
+which fswatch >/dev/null
+if [ $? = 0 ]; then
+  fswatch=true
+fi
+
+if [ $fswatch ]; then
+  echo "* Detected fswatch"
+  fswatch_cmd="fswatch -1 \`find . -type d -depth 1 -not -name '.*' -not -name '_*' -exec printf ' "{}"' \;\`"
+  mix_test_cmd="while :; do; $mix_test_cmd; $fswatch_cmd; done"
+else
+  echo "* Running tests/examples once -- install fswatch to run them continuously"
+fi
+
+$tmux_cmd send-keys -t $session_name:1.2 "$bundle_exec $mix_test_cmd" C-m
+$tmux_cmd split-window -v -p 40 -t $session_name:1.2
 
 if [ -d lib ] && [ -d priv ] && [ -d web ]; then
   echo "* Detected Phoenix project"
