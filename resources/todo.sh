334a9a5d4e428a783025784721f8cd9830a6cbb6
Nils Jonsson <nils@alumni.rice.edu>
2020-07-25 21:08:43 -0500

    Add `todo --filename` option and refactor 'printf' statements

--- resources/todo.sh
+++ resources/todo.sh
@@ -32,19 +32,28 @@ todo_help() {
   local filename="${filename/$HOME/~}"
   local program=$(basename $0)
 
-  printf "A simple to-do list, stored in \e[4m$file\e[24m\n\n"
+  printf "A simple to-do list\n"
+  printf "\n"
 
-  printf "  \e[1m$program Something to do\e[22m   Adds \"Something to do\" as new item in the to-do list\n\n"
+  printf "  \e[1m$program Something to do\e[22m   Adds \"Something to do\" as new item in the to-do list\n"
+  printf "\n"
 
   printf "  \e[1m$program --edit\e[22m            Opens the to-do list in your editor\n"
   printf "  \e[1m$program -e\e[22m\n"
-  printf "  \e[1m$program\e[22m\n\n"
+  printf "  \e[1m$program\e[22m\n"
+  printf "\n"
+
+  printf "  \e[1m$program --filename\e[22m        Displays the to-do list filename\n"
+  printf "  \e[1m$program -f\e[22m\n"
+  printf "\n"
 
   printf "  \e[1m$program --list\e[22m            Lists to-do items\n"
-  printf "  \e[1m$program -l\e[22m\n\n"
+  printf "  \e[1m$program -l\e[22m\n"
+  printf "\n"
 
   printf "  \e[1m$program --open\e[22m            Opens the to-do list in the application associated with \e[4m$filename\e[24m\n"
-  printf "  \e[1m$program -o\e[22m\n\n"
+  printf "  \e[1m$program -o\e[22m\n"
+  printf "\n"
 
   printf "  \e[1m$program --help\e[22m            Displays this help message\n"
   printf "  \e[1m$program -h\e[22m\n"
@@ -72,6 +81,9 @@ todo_open() {
 case "$*" in
   --edit | -e | '' )
     todo_edit
+    ;;
+  --filename | -f )
+    todo_filename
     ;;
   --help | -h )
     todo_help
