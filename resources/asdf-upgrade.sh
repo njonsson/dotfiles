d3d34806d894a0e7c02e9be66e7b4e4a64f94d12
Nils Jonsson <nils@alumni.rice.edu>
2020-09-26 17:49:17 -0500

    Enhance `asdf-upgrade` to detect installed tools

--- resources/asdf-upgrade.sh
+++ resources/asdf-upgrade.sh
@@ -2,39 +2,38 @@
 
 set -o pipefail
 
-printf "*** Updating \e[4masdf\e[24m ...\n"
+printf "*** Updating \e[4masdf\e[24m and its plugins ...\n"
 printf "\e[2m"
-asdf update \
+asdf update                   \
+  && printf "\e[0m"           \
+  && printf "\e[2m"           \
   && asdf plugin-update --all \
-  && printf "\e[22m" \
-  && printf "*** Successfully updated \e[4masdf\e[24m\n"
+  && printf "\e[0m"           \
+  && printf "*** Successfully updated \e[4masdf\e[24m and its plugins\n"
 
-echo "\n*** Listing available Elixir versions ..."
-# Include only versions rather than branches.
-# Exclude release-candidate versions.
-asdf list-all elixir \
-  | grep '^\d' \
-  | grep --invert-match '\brc\b' \
-  | tail -3
+asdf current 2>&1 | while read entry; do
+  tool_and_current_version=($entry)
+  tool=${tool_and_current_version[0]}
+  current_version=${tool_and_current_version[1]}
+  latest_version=$(asdf latest $tool)
+  if [ -z "$latest_version" ]; then
+    printf "*** Current \e[4m$tool\e[24m version "
+    printf "(\e[4m$current_version\e[24m) still installed —— "
+    printf "no latest version available\n"
+    continue
+  fi
 
-echo "\n*** Listing available Erlang versions ..."
-asdf list-all erlang \
-  | tail -3
+  if [ "$current_version" = "$latest_version" ]; then
+    printf "*** Latest \e[4m$tool\e[24m (\e[4m$latest_version\e[24m) "
+    printf "already installed\n"
+    continue
+  fi
 
-echo "\n*** Listing available Java versions ..."
-asdf list-all java \
-  | grep oracle \
-  | tail -3
-
-echo "\n*** Listing available Ruby versions ..."
-# Include only MRI versions.
-asdf list-all ruby \
-  | grep --extended-regexp --line-regexp '[0-9.]+' \
-  | tail -3
-
-echo "\n*** Listing available Rust versions ..."
-asdf list-all rust \
-  | tail -3
-
-printf "\n*** Active versions via \e[4masdf\e[24m:\n"
-asdf current
+  printf "*** Installing \e[4m$tool\e[24m \e[4m$latest_version\e[24m ...\n"
+  printf "\e[2m"
+  asdf install $tool $latest_version        \
+    && asdf global $tool $latest_version    \
+    && printf "\e[0m"                       \
+    && printf "*** Successfully installed " \
+    && printf "\e[4m$tool\e[24m \e[4m$latest_version\e[24m\n"
+done
