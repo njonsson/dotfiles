fda57b5b87019bf3f62fd4695818e7ad68ce40fb
Nils Jonsson <nils@alumni.rice.edu>
2021-09-26 16:26:40 -0500

    Enhance `epoch2iso` and `iso2epoch` to handle stdin

--- resources/epoch2iso.sh
+++ resources/epoch2iso.sh
@@ -9,6 +9,57 @@ function format_date() {
   date -ju -f $input_format $input +$output_format
 }
 
+function process_argument() {
+  case "$1" in
+    [0-9])
+      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
+      ;;
+    [0-9][0-9])
+      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
+      ;;
+    [0-9][0-9][0-9])
+      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
+      ;;
+    [0-9][0-9][0-9][0-9])
+      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
+      ;;
+    [0-9][0-9][0-9][0-9][0-9])
+      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
+      ;;
+    [0-9][0-9][0-9][0-9][0-9][0-9])
+      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
+      ;;
+    [0-9][0-9][0-9][0-9][0-9][0-9][0-9])
+      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
+      ;;
+    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
+      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
+      ;;
+    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
+      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
+      ;;
+    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
+      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
+      ;;
+    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
+      format_date ${1:0:10} %s %Y-%m-%dT%H:%M:%S.${1:10:3}Z
+      ;;
+    *)
+      return 1
+      ;;
+  esac
+}
+
+function process_stdin() {
+  while read -r; do
+    if [ -z "$REPLY" ]; then
+      echo
+    else
+      process_argument "$REPLY" || return 1
+    fi
+  done
+}
+
 function usage() {
   local now_in_seconds=$(
     date -ju +%s
@@ -18,47 +69,11 @@ function usage() {
   printf "       \e[1m$(basename $0) \e[22;4m$now_in_milliseconds\e[24m\n" >&2
 }
 
-if [ $# -ne 1 ]; then
-  usage
-  exit 1
+if [ -p /dev/stdin ]; then
+  [ $# -eq 0 ] && process_stdin && exit 0
+else
+  [ $# -eq 1 ] && process_argument "$1" && exit 0
 fi
 
-case "$1" in
-  [0-9])
-    format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-    ;;
-  [0-9][0-9])
-    format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-    ;;
-  [0-9][0-9][0-9])
-    format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-    ;;
-  [0-9][0-9][0-9][0-9])
-    format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-    ;;
-  [0-9][0-9][0-9][0-9][0-9])
-    format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-    ;;
-  [0-9][0-9][0-9][0-9][0-9][0-9])
-    format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-    ;;
-  [0-9][0-9][0-9][0-9][0-9][0-9][0-9])
-    format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-    ;;
-  [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
-    format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-    ;;
-  [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
-    format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-    ;;
-  [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
-    format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-    ;;
-  [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
-    format_date ${1:0:10} %s %Y-%m-%dT%H:%M:%S.${1:10:3}Z
-    ;;
-  *)
-    usage
-    exit 1
-    ;;
-esac
+usage
+exit 1
