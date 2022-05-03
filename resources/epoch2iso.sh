45cf8bfe32d8a41cdd80b963190ab25405ee4a66
Nils Jonsson <nils@alumni.rice.edu>
2022-05-02 21:29:10 -0500

    Make epoch values floating-point, and support additional ISO 8601 expressions
    Previously we followed the convention of some platforms which interpret
    13-digit epoch expressions as 10 digits with a fraction of three
    decimal places. This has a bias toward epoch values near the turn of
    the 21st Century, compromising support for all positive epoch values.
    Here we backpedal on this compromise.
    
    We also add support for ISO 8601 expressions `YYYY`, `YYYY-MM`,
    `YYYY-MM-DD`, `YYYY-MM-DDThhZ`, `YYYY-MM-DDThh:mmZ`, `YYYYMMDD`,
    `YYYYMMDDThhZ`, and `YYYYMMDDThhmmZ`.
    
    Fractional seconds can be omitted or specified in arbitrary quantities
    in both epoch and ISO 8601 expressions.

--- resources/epoch2iso.sh
+++ resources/epoch2iso.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 set -Eeuo pipefail
+shopt -s extglob
 
 function format_date() {
   local input="$1"
@@ -11,38 +12,13 @@ function format_date() {
 
 function process_argument() {
   case "$1" in
-    [0-9])
+    +([0-9]))
       format_date $1 %s %Y-%m-%dT%H:%M:%SZ
       ;;
-    [0-9][0-9])
-      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-      ;;
-    [0-9][0-9][0-9])
-      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-      ;;
-    [0-9][0-9][0-9][0-9])
-      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-      ;;
-    [0-9][0-9][0-9][0-9][0-9])
-      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-      ;;
-    [0-9][0-9][0-9][0-9][0-9][0-9])
-      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-      ;;
-    [0-9][0-9][0-9][0-9][0-9][0-9][0-9])
-      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-      ;;
-    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
-      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-      ;;
-    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
-      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-      ;;
-    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
-      format_date $1 %s %Y-%m-%dT%H:%M:%SZ
-      ;;
-    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
-      format_date ${1:0:10} %s %Y-%m-%dT%H:%M:%S.${1:10:3}Z
+    +([0-9]).+([0-9]))
+      local int=${1%.*}
+      local frac=${1#*.}
+      format_date $int %s %Y-%m-%dT%H:%M:%S."$frac"Z
       ;;
     *)
       return 1
@@ -64,9 +40,8 @@ function usage() {
   local now_in_seconds=$(
     date -ju +%s
   )
-  local now_in_milliseconds="$now_in_seconds"000
-  printf "Usage: \e[1m$(basename $0) \e[22;4m$now_in_seconds\e[24m\n"      >&2
-  printf "       \e[1m$(basename $0) \e[22;4m$now_in_milliseconds\e[24m\n" >&2
+  printf "Usage: \e[1m$(basename $0) \e[22;4m$now_in_seconds\e[24m\n"       >&2
+  printf "       \e[1m$(basename $0) \e[22;4m$now_in_seconds.0\e[24m ...\n" >&2
 }
 
 if [ -p /dev/stdin ]; then
--- resources/iso2epoch.sh
+++ resources/iso2epoch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 set -Eeuo pipefail
+shopt -s extglob
 
 function format_date() {
   local input="$1"
@@ -11,35 +12,43 @@ function format_date() {
 
 function process_argument() {
   case "$1" in
+    [0-9][0-9][0-9][0-9])
+      format_date $1-01-01T00:00:00Z %Y-%m-%dT%H:%M:%SZ %s
+      ;;
+    [0-9][0-9][0-9][0-9]-[0-9][0-9])
+      format_date $1-01T00:00:00Z %Y-%m-%dT%H:%M:%SZ %s
+      ;;
+    [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])
+      format_date $1T00:00:00Z %Y-%m-%dT%H:%M:%SZ %s
+      ;;
+    [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]Z)
+      format_date ${1:0:13}:00:00Z %Y-%m-%dT%H:%M:%SZ %s
+      ;;
+    [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]Z)
+      format_date ${1:0:16}:00Z %Y-%m-%dT%H:%M:%SZ %s
+      ;;
     [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]Z)
       format_date $1 %Y-%m-%dT%H:%M:%SZ %s
       ;;
-    [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9].Z)
-      format_date ${1:0:19}Z %Y-%m-%dT%H:%M:%SZ %s000
+    [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9].+([0-9])Z)
+      local frac_with_z=${1#*.}
+      format_date ${1%.*}Z %Y-%m-%dT%H:%M:%SZ %s.${frac_with_z%Z}
       ;;
-    [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9]Z)
-      format_date ${1:0:19}Z %Y-%m-%dT%H:%M:%SZ %s${1:20:1}00
+    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
+      format_date $1T000000Z %Y%m%dT%H%M%SZ %s
       ;;
-    [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9]Z)
-      format_date ${1:0:19}Z %Y-%m-%dT%H:%M:%SZ %s${1:20:2}0
+    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]T[0-9][0-9]Z)
+      format_date ${1:0:11}0000Z %Y%m%dT%H%M%SZ %s
       ;;
-    [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9]Z)
-      format_date ${1:0:19}Z %Y-%m-%dT%H:%M:%SZ %s${1:20:3}
+    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]T[0-9][0-9][0-9][0-9]Z)
+      format_date ${1:0:13}00Z %Y%m%dT%H%M%SZ %s
       ;;
     [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]T[0-9][0-9][0-9][0-9][0-9][0-9]Z)
       format_date $1 %Y%m%dT%H%M%SZ %s
       ;;
-    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]T[0-9][0-9][0-9][0-9][0-9][0-9].Z)
-      format_date ${1:0:15}Z %Y%m%dT%H%M%SZ %s000
-      ;;
-    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]T[0-9][0-9][0-9][0-9][0-9][0-9].[0-9]Z)
-      format_date ${1:0:15}Z %Y%m%dT%H%M%SZ %s${1:16:1}00
-      ;;
-    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]T[0-9][0-9][0-9][0-9][0-9][0-9].[0-9][0-9]Z)
-      format_date ${1:0:15}Z %Y%m%dT%H%M%SZ %s${1:16:2}0
-      ;;
-    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]T[0-9][0-9][0-9][0-9][0-9][0-9].[0-9][0-9][0-9]Z)
-      format_date ${1:0:15}Z %Y%m%dT%H%M%SZ %s${1:16:3}
+    [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]T[0-9][0-9][0-9][0-9][0-9][0-9].+([0-9])Z)
+      local frac_with_z=${1#*.}
+      format_date ${1%.*}Z %Y%m%dT%H%M%SZ %s.${frac_with_z%Z}
       ;;
     *)
       return 1
@@ -58,14 +67,20 @@ function process_stdin() {
 }
 
 function usage() {
-  printf "Usage: \e[1m$(basename $0) \e[22;4mYYYY\e[24;1m-\e[22;4mMM\e[24;1m-\e[22;4mDD\e[24;1mT\e[22;4mhh\e[24;1m:\e[22;4mmm\e[24;1m:\e[22;4mss\e[24;1mZ\e[22m\n"                     >&2
-  printf "       \e[1m$(basename $0) \e[22;4mYYYY\e[24;1m-\e[22;4mMM\e[24;1m-\e[22;4mDD\e[24;1mT\e[22;4mhh\e[24;1m:\e[22;4mmm\e[24;1m:\e[22;4mss\e[24;1m.\e[22;4ms\e[24;1mZ\e[22m\n"   >&2
-  printf "       \e[1m$(basename $0) \e[22;4mYYYY\e[24;1m-\e[22;4mMM\e[24;1m-\e[22;4mDD\e[24;1mT\e[22;4mhh\e[24;1m:\e[22;4mmm\e[24;1m:\e[22;4mss\e[24;1m.\e[22;4mss\e[24;1mZ\e[22m\n"  >&2
-  printf "       \e[1m$(basename $0) \e[22;4mYYYY\e[24;1m-\e[22;4mMM\e[24;1m-\e[22;4mDD\e[24;1mT\e[22;4mhh\e[24;1m:\e[22;4mmm\e[24;1m:\e[22;4mss\e[24;1m.\e[22;4msss\e[24;1mZ\e[22m\n" >&2
-  printf "       \e[1m$(basename $0) \e[22;4mYYYYMMDD\e[24;1mT\e[22;4mhhmmss\e[24;1mZ\e[22m\n"                                                                                         >&2
-  printf "       \e[1m$(basename $0) \e[22;4mYYYYMMDD\e[24;1mT\e[22;4mhhmmss\e[24;1m.\e[22;4ms\e[24;1mZ\e[22m\n"                                                                       >&2
-  printf "       \e[1m$(basename $0) \e[22;4mYYYYMMDD\e[24;1mT\e[22;4mhhmmss\e[24;1m.\e[22;4mss\e[24;1mZ\e[22m\n"                                                                      >&2
-  printf "       \e[1m$(basename $0) \e[22;4mYYYYMMDD\e[24;1mT\e[22;4mhhmmss\e[24;1m.\e[22;4msss\e[24;1mZ\e[22m\n"                                                                     >&2
+  printf "Usage: \e[1m$(basename $0) \e[22;4mYYYY\e[24m\n"                                                                                                                                >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYY\e[24;1m-\e[22;4mMM\e[24m\n"                                                                                                             >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYY\e[24;1m-\e[22;4mMM\e[24;1m-\e[22;4mDD\e[24m\n"                                                                                          >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYY\e[24;1m-\e[22;4mMM\e[24;1m-\e[22;4mDD\e[24;1mT\e[22;4mhh\e[24;1mZ\e[22m\n"                                                              >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYY\e[24;1m-\e[22;4mMM\e[24;1m-\e[22;4mDD\e[24;1mT\e[22;4mhh\e[24;1m:\e[22;4mmm\e[24;1mZ\e[22m\n"                                           >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYY\e[24;1m-\e[22;4mMM\e[24;1m-\e[22;4mDD\e[24;1mT\e[22;4mhh\e[24;1m:\e[22;4mmm\e[24;1m:\e[22;4mss\e[24;1mZ\e[22m\n"                        >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYY\e[24;1m-\e[22;4mMM\e[24;1m-\e[22;4mDD\e[24;1mT\e[22;4mhh\e[24;1m:\e[22;4mmm\e[24;1m:\e[22;4mss\e[24;1m.\e[22;4ms\e[24;1mZ\e[22m\n"      >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYY\e[24;1m-\e[22;4mMM\e[24;1m-\e[22;4mDD\e[24;1mT\e[22;4mhh\e[24;1m:\e[22;4mmm\e[24;1m:\e[22;4mss\e[24;1m.\e[22;4mss\e[24;1mZ\e[22m ...\n" >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYYMMDD\e[24m\n"                                                                                                                            >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYYMMDD\e[24;1mT\e[22;4mhh\e[24;1mZ\e[22m\n"                                                                                                >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYYMMDD\e[24;1mT\e[22;4mhhmm\e[24;1mZ\e[22m\n"                                                                                              >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYYMMDD\e[24;1mT\e[22;4mhhmmss\e[24;1mZ\e[22m\n"                                                                                            >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYYMMDD\e[24;1mT\e[22;4mhhmmss\e[24;1m.\e[22;4ms\e[24;1mZ\e[22m\n"                                                                          >&2
+  printf "       \e[1m$(basename $0) \e[22;4mYYYYMMDD\e[24;1mT\e[22;4mhhmmss\e[24;1m.\e[22;4mss\e[24;1mZ\e[22m ...\n"                                                                     >&2
 }
 
 if [ -p /dev/stdin ]; then
